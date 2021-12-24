8386495186be4ef168921995c5ae9176bce90430
Dung2781993@gmail.com <Dung2781993@gmail.com>
2021-12-24 23:45:38 +1100

    First commit

---
+++ Test1/Repository/StudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test1.Repository
+{
+    public class StudentRepository : GenericRepository<Student>
+    {
+        public List<Student> Search(string name)
+        {
+            if(string.IsNullOrEmpty(name)) return null;
+            var studens = this.SelectAll().ToList(); 
+            //Get student by firstname or last name
+            return studens.Where(item => item.FirstName.ToLower().Contains(name.ToLower()) || item.LastName.ToLower().Contains(name.ToLower())).ToList();
+        }
+    }
+}
